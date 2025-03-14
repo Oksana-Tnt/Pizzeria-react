007c498c91d5eeea25b963486637fc83a8bcdc43
Oksana-Tnt <oksanatantsiura@gmail.com>
2025-03-14 18:35:57 +0100

    add authHelper.js

---
+++ src/utils/authHelper.js
@@ -0,0 +1,54 @@
+import apiConfig from "@config/apiConfig";
+import axios from "axios";
+
+let options = {
+        headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${getAccessToken()}`
+        }        
+    }
+
+export async function pushToDb(route, body, hasToken = false) {
+    if (!hasToken && !isUserLoggedIn()) {
+        options = {};
+    }
+        return (await axios.post(`${apiConfig.baseUrl}/${route}`, body, options)).data;
+ }
+
+export async function fetchFromDb(route, hasToken = false) {
+    if (!hasToken && !isUserLoggedIn()) {
+        options = {};
+    }
+    return (await axios.get(`${apiConfig.baseUrl}/${route}`, options)).data;
+}    
+    
+
+export async function deleteFromDb(route, id) {
+        return (await axios.delete(`${apiConfig.baseUrl}/${route}/${id}`, options)).data
+}
+
+export function getAccessToken(){
+    const accessDataString = localStorage.getItem('access_data');
+    if(!accessDataString) return null;
+
+    const accessData = JSON.parse(accessDataString);
+
+    return accessData.access_token
+}
+
+export function getUser(){
+    const accessDataString = localStorage.getItem('access_data');
+    if(!accessDataString) return null;
+
+    const accessData = JSON.parse(accessDataString);
+
+    return accessData.user
+}
+
+export function isUserLoggedIn(){
+    return getUser() ? true : false;
+}
+export function logout() {
+   
+    localStorage.removeItem('access_data');
+}
